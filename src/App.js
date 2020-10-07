12d7e3fa8bf22cb8d15ff1685a050c095fe9846b
ghadeer <apps02977@gmail.com>
2020-10-07 13:13:05 +0300

    Cards component refactor

--- src/App.js
+++ src/App.js
@@ -2,31 +2,24 @@ import React from "react";
 import "./App.css";
 //Data
 import cards from "./data";
-
+//Components
+import Card from "./Components/Card"; /* 1 */
 // Card Back Image
 import cardBack from "./images/CardBack.jpg";
 
 
 function App() {
-  return <div className="App border my-5"> <div className="App border my-5">
-  <div className="container">
-    <div className="row">
-      {/* 1 */}
-      <div className="col-3 my-1">
-        {/* 2 */}
-        <img
-          className="mx-auto"
-          src={cardBack}
-          //   used percentages instead of pixels to be responsive with the screen size
-          height="100%"
-          width="100%"
-          key="back"
-        />
-        {/* 3 */}
+  let cardsGrid = cards.map(card => (
+    <Card key={card.id} card={card} />
+  )); /* 2, 3 */
+
+  return (
+    <div className="App border my-5">
+      <div className="container">
+        <div className="row">{cardsGrid}</div>
       </div>
     </div>
-  </div>
-</div></div>;
+  );
 }
 
 export default App;
